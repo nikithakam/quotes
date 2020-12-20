24cf69c9ea75947bbc41ac4dadfd622fdbfbc5b0
ndkamat <ndkamath@umich.edu>
2020-12-20 15:42:51 -0500

    new changes

--- app.js
+++ app.js
@@ -13,9 +13,27 @@ function getColor() {
   }
 
   document.body.onkeyup = function(e) {
-    if (e.keyCode == 191) {
+    if (e.keyCode == 13) {
       setBackground();
     }
   };
 
-  
+  function getWeather() {
+      let temperature = document.getElementById("temperature");
+      let description = document.getElementById("description");
+      let location = document.getElementById("location");
+      let api = "https://api.openweathermap.org/data/2.5/weather";
+      let apiKey = "d7b693f0b8369328c5cb719d435a8cee";
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    console.log(position);
+   }
+   
+   function error() {
+    console.log("error");
+   }
+
+   getWeather();
